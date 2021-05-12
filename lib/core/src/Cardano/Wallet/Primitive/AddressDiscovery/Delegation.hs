a931f5ded1275d17ae8cfc06114708a945510bbe
Johannes Lund <johannes.lund@iohk.io>
2021-05-12 14:42:24 +0200

    Tweak doc comments

--- lib/core/src/Cardano/Wallet/Primitive/AddressDiscovery/Delegation.hs
+++ lib/core/src/Cardano/Wallet/Primitive/AddressDiscovery/Delegation.hs
@@ -11,6 +11,7 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 
+-- | Module for `DelegationState`.
 module Cardano.Wallet.Primitive.AddressDiscovery.Delegation
     ( -- * Creation
       DelegationState (..)
@@ -70,9 +71,61 @@ import Quiet
 import qualified Cardano.Wallet.Primitive.Types.TokenBundle as TB
 
 --------------------------------------------------------------------------------
--- Stake Key State
+-- Delegation State
 --------------------------------------------------------------------------------
 
+-- | Delegation state
+--
+-- === Goals
+-- 1. Allow a wallet to have an arbitrary number of stake keys
+-- 2. Ensure those stake keys can be automatically discovered on-chain
+-- 3. Ensure the wallet is always aware of all stake keys it registers, even in
+-- the case of concurrent user actions on multiple wallet instances, and old
+-- wallet software.
+--
+-- === Diagram
+-- Diagram of states, where the list denotes active (registered /and/ delegating)
+-- keys.
+--
+-- Here we assume the minUTxOValue is 1 ada.
+--
+-- Note that intermediate steps for the `PointerUTxO` should be skipped within a
+-- transaction.
+-- E.g. to transition from [] to [0,1,2] we should deposit 1 ada to key 3,
+-- skipping key 2.
+--
+-- See the implementation of `setPortfolioOf` and `applyTx` for more details.
+--
+-- @
+-- ┌────────────────────┐           ┌────────────────────┐                     ┌────────────────────┐            ┌─────────────────────┐
+-- │                    │           │                    │                     │                    │            │                     │
+-- │                    │           │                    │       Pointer       │                    │            │                     │
+-- │                    │──────────▶│                    │ ──────deposit──────▶│                    │ ─────────▶ │                     │ ─────────▶
+-- │                    │           │                    │                     │       [0,1]        │            │       [0,1,2]       │
+-- │         []         │           │        [0]         │                     │1 ada held by key 2 │            │ 1 ada held by key 3 │
+-- │                    │           │                    │                     │                    │            │                     │
+-- │                    │           │                    │       Pointer       │                    │            │                     │
+-- │                    │◀──────────│                    │ ◀─────deposit ──────│                    │◀────────── │                     │◀──────────
+-- │                    │           │                    │       returned      │                    │            │                     │
+-- └────────────────────┘◀──┐       └────────────────────┘                     └────────────────────▲            ▲─────────────────────▲            ▲
+--                          └───┐                                                     │       ▲     └─┐         ┌┘      │       ▲      └─┐         ┌┘
+-- Normal states                └───┐                                                 │       │       └─┐     ┌─┘       │       │        └─┐     ┌─┘
+--                                  └───┐                                             │       │         └─┐ ┌─┘         │       │          └─┐ ┌─┘
+-- ╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳└───┐╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳╳│╳╳╳╳╳╳╳│╳╳╳╳╳╳╳╳╳╳╳│ │╳╳╳╳╳╳╳╳╳╳╳│╳╳╳╳╳╳╳│╳╳╳╳╳╳▶     ├─┤
+--                                          └───┐                                     │       │         ┌─┘ └─┐         │       │          ┌─┘ └─┐
+-- States caused by                             └────┐Pointer                         ▼       │       ┌─┘     └─┐       ▼       │        ┌─┘     └─┐
+-- old wallet                                        └deposit                  ┌────────────────────┐─┘         └┬─────────────────────┐─┘         └─
+-- de-registering                                     returned─┐               │                    │            │                     │
+-- stake-key 0                                                 └────┐          │                    │ ─────────▶ │                     │ ─────────▶
+-- of multi-stake                                                   └────┐     │                    │            │                     │
+-- key wallet                                                            └──── │        [1]         │            │        [1,2]        │
+--                                                                             │1 ada held by key 2 │            │ 1 ada held by key 3 │
+--                                                                             │                    │            │                     │
+--                                                                             │                    │◀────────── │                     │◀──────────
+--                                                                             │                    │            │                     │
+--                                                                             │                    │            │                     │
+--                                                                             └────────────────────┘            └─────────────────────┘
+-- @
 data DelegationState k = DelegationState
     { -- | The account public key from which the stake keys should be derived.
       rewardAccountKey :: k 'AccountK XPub
@@ -80,11 +133,11 @@ data DelegationState k = DelegationState
       -- | The next unregistered stake key.
       --
       -- The active stake keys can be described as the half-open interval
-      --    @@ [0, nextKeyIx ) @@
+      --    @[0, nextKeyIx )@
       -- having a length of @nextKeyIx@.
       --
-      -- E.g. a state with nextKeyIx=0 has no keys, and nextKeyIx=2 has the keys
-      -- [0,1].
+      -- E.g. a state with @nextKeyIx=0@ has no keys, and @nextKeyIx=2@ has the keys
+      -- @[0,1]@.
     , nextKeyIx :: !(Index 'Soft 'AddressK)
 
       -- | By threading a long a "pointer UTxO", we leverage the ledger rules to
@@ -95,7 +148,7 @@ data DelegationState k = DelegationState
       -- | For compatibility with single-stake-key wallets, we track whether
       -- stake key 0 is registered or not separately.
       --
-      -- See the implementaiton of @applyTx@ for how it is used.
+      -- See the implementaiton of `applyTx` for how it is used.
     , isKey0Reg :: Bool
     } deriving (Generic)
 
@@ -150,11 +203,11 @@ data PointerUTxO = PointerUTxO { pTxIn :: TxIn, pCoin :: Coin }
     deriving (Generic, Eq, Show)
     deriving anyclass NFData
 
--- | Returns the index corresponding to the payment key the @PointerUTxO@
+-- | Returns the index corresponding to the payment key the `PointerUTxO`
 -- should be locked with.
 --
--- Our current implementation we require the @PointerUTxO@ to be created in the
--- @@[0] -> [0,1] transition@@, i.e. @@nextKeyIx 1 -> nextKeyIx 2@@.
+-- Our current implementation we require the `PointerUTxO` to be created in the
+-- @[0] -> [0,1] transition@, i.e. @nextKeyIx 1 -> nextKeyIx 2@.
 pointerIx
     :: DelegationState k
     -> Maybe (Index 'Soft 'AddressK)
@@ -193,9 +246,6 @@ data Tx = Tx
     deriving (Eq, Generic)
     deriving Show via (Quiet Tx)
 
--- FIXME: This logic might belong in the DBLayer, in which this really is the
--- MVar implementation and out of place.
-
 data Cert
     = RegisterKey RewardAccount
     | Delegate RewardAccount
@@ -205,15 +255,13 @@ data Cert
     | DeRegisterKey RewardAccount
     deriving (Eq, Show, Generic)
 
+-- Given a `DelegationState`, produce a `Tx` registering or de-registering
+-- stake-keys, in order to have @n@ stake-keys.
 --
+-- E.g. @setPortfolioOf s0 _ _ 2@ creates a tx which after application causes
+-- the state to have @activeKeys == [0,1]@
 --
--- a portfolio of 3 keys:
--- [0, 1, 2]
--- should have a nextKeyIx of 3
---
--- portfolio of 0 keys, should have a nextKeyIx of 0
---
--- FIXME: toEnum safety
+-- Returns @Nothing@ if the target @n@ is already reached.
 setPortfolioOf
     :: (SoftDerivation k, ToRewardAccount k)
     => DelegationState k
@@ -326,7 +374,6 @@ applyTx (Tx cs ins outs) h s0 =
         DeRegisterKey k
             | Just k == lastActiveKey s && hitGap k -> pred . pred -- i.e. to 0
             | Just k == lastActiveKey s -> pred
-              -- TODO: Reason explicitly about the safety of this @pred@.
             | otherwise                 -> id
       where
         modifyKey0 s' = case c of
@@ -349,11 +396,21 @@ applyTx (Tx cs ins outs) h s0 =
 
 -- | All stake keys worth listing to the user.
 --
--- Now returns
+-- May include:
 -- 1. Active stake keys
--- 2. The next unregistered stake key
+-- 2. The next un-active key
 --
--- NOTE: Now it rarely tracks unexpected stake key registrations.
+-- NOTE: In theory we might want also present stake keys that are unexpectedly
+-- registered, as they could be de-registered to reclaim the deposit, but this
+-- should in-practice never happen.
+--
+-- If @sn@ denotes the state with @n@ registered and delegating keys:
+-- >>> presentableKeys s0
+-- [0]
+-- >>> presentableKeys s1
+-- [0, 1]
+-- >>> presentableKeys s2
+-- [0, 1, 2]
 presentableKeys :: SoftDerivation k => DelegationState k -> [k 'AddressK XPub]
 presentableKeys s = case lastActiveIx s of
     Just i -> map (keyAtIx s) [minBound .. (succ i)]
@@ -361,17 +418,23 @@ presentableKeys s = case lastActiveIx s of
 
 -- Keys meant to be used in addresses.
 --
--- NOTE: Returned keys may not necessarily be registered and delegating. If the
--- user has no registered stake key, the first key (index 0) should still be
--- used in addresses, and is deemed "presentable".
+-- If @sn@ denotes the state with @n@ registered and delegating keys:
+-- >>> usableKeys s0
+-- [0]
+-- >>> usableKeys s1
+-- [0]
+-- >>> usableKeys s2
+-- [0, 1]
+--
+-- Note that for @s0@, we have no active stake keys, but we still want to use
+-- key 0 as part of addresses.
 usableKeys :: SoftDerivation k => DelegationState k -> [k 'AddressK XPub]
 usableKeys s = case lastActiveIx s of
     Just i -> map (keyAtIx s) [minBound .. i]
     Nothing -> [keyAtIx s minBound]
 
--- | For testing.
+-- | For testing. Returns all registered and delegating stake keys.
 activeKeys :: SoftDerivation k => DelegationState k -> [k 'AddressK XPub]
 activeKeys s = case lastActiveIx s of
     Just i -> map (keyAtIx s) [minBound .. i]
     Nothing -> []
-
